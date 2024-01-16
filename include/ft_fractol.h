5d404178e5a3a81e066c9cd17dab7c157f951e44
Margareth Dias <mdias@c1r7p3.42sp.org.br>
2024-01-15 23:04:55 -0300

    Inicio do Fractol, desenhei um quadrado e depois um tabuleiro

---
+++ include/ft_fractol.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fractol.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdias <mdias@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 19:27:44 by mdias             #+#    #+#             */
+/*   Updated: 2024/01/15 21:35:10 by mdias            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FRACTOL_H
+# define FT_FRACTOL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "../.lib/MLX42/include/MLX42/MLX42.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+# define WIDTH 512
+# define HEIGHT 512
+
+// /**
+//  * Main MLX handle, carries important data in regards to the program.
+//  * @param window The window itself.
+//  * @param context Abstracted opengl data.
+//  * @param width The width of the window.
+//  * @param height The height of the window.
+//  * @param delta_time The time difference between the previous frame and the current frame.
+//  */
+// typedef struct mlx
+// {
+// 	void*		window;
+// 	void*		context;
+// 	int32_t		width;
+// 	int32_t		height;
+// 	double		delta_time;
+// }	mlx_t;
+
+#endif
