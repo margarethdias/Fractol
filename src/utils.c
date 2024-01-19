4b929ea53533be64c3c04e9704d91cc05fb437b2
Margareth Dias <mdias@c2r4p4.42sp.org.br>
2024-01-19 20:22:51 -0300

    inicio da struct

--- src/utils.c
+++ src/utils.c
@@ -1,19 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdias <mdias@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 19:30:09 by mdias             #+#    #+#             */
-/*   Updated: 2024/01/19 19:34:20 by mdias            ###   ########.fr       */
+/*   Created: 2024/01/18 20:06:45 by mdias             #+#    #+#             */
+/*   Updated: 2024/01/19 19:18:52 by mdias            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/ft_fractol.h"
 
-int main(int argc, char **argv)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (argc == 2 && !ft_strncmp(argv[1], "mandelbrot", 11) 
-		|| argc == 2 && !ft_strncmp(argv[1], "julia", 6))
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0' && s2[i] != '\0') && (i < n - 1))
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (s != NULL)
+	{
+		i = 0;
+		while (s[i])
+		{
+			ft_putchar_fd(s[i], fd);
+			i++;
+		}
+	}
 }
